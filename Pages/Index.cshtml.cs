e8363ceb05419f7bcc3e1a743a501926e8b16b43
Gutemberg dos Santos Bezerra <papelpoint.contato@hotmail.com>
2024-05-23 13:54:22 -0300

    Index
    Criação do body e teste dos redirecionamento dos botões das 3 forma (Amém!!)

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,16 +5,14 @@ namespace PortalArcomix.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
+        }
 
+        public IActionResult OnPost()
+        {
+            // Redirect to the Privacy page
+            return RedirectToPage("/Privacy");
         }
     }
 }
