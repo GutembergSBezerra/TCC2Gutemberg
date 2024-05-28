c4005fb65ba5539904d867387c8046631c2f21a9
Gutemberg dos Santos Bezerra <papelpoint.contato@hotmail.com>
2024-05-28 06:54:45 -0300

    integração primeiro card parcial PRODUTOVI

--- Pages/ProdutoVI.cshtml.cs
+++ Pages/ProdutoVI.cshtml.cs
@@ -32,7 +32,7 @@ namespace PortalArcomix.Pages
             using (var conn = new SqlConnection(connectionString))
             {
                 var queryProduto = @"
-                    SELECT DescricaoProduto, CNPJ
+                    SELECT DescricaoProduto, CNPJ, GestorCompras, Importado
                     FROM Tbl_Produto
                     WHERE ID = @Id";
 
@@ -48,6 +48,8 @@ namespace PortalArcomix.Pages
                             ProdutoDetails.ID = id.Value;
                             ProdutoDetails.DescricaoProduto = reader["DescricaoProduto"].ToString();
                             ProdutoDetails.CNPJ = reader["CNPJ"].ToString();
+                            ProdutoDetails.GestorCompras = reader["GestorCompras"].ToString();
+                            ProdutoDetails.Importado = reader["Importado"] != DBNull.Value && (bool)reader["Importado"];
                         }
                     }
                     await conn.CloseAsync();
@@ -56,7 +58,7 @@ namespace PortalArcomix.Pages
                 if (ProdutoDetails.CNPJ != null)
                 {
                     var queryFornecedor = @"
-                        SELECT RazaoSocial
+                        SELECT RazaoSocial, TipoFornecedor
                         FROM Tbl_Fornecedor
                         WHERE CNPJ = @CNPJ";
 
@@ -70,8 +72,10 @@ namespace PortalArcomix.Pages
                             if (await reader.ReadAsync())
                             {
                                 FornecedorDetails.RazaoSocial = reader["RazaoSocial"].ToString();
+                                FornecedorDetails.TipoFornecedor = reader["TipoFornecedor"].ToString();
                             }
                         }
+                        await conn.CloseAsync();
                     }
                 }
             }
@@ -92,13 +96,17 @@ namespace PortalArcomix.Pages
             {
                 var queryProduto = @"
                     UPDATE Tbl_Produto
-                    SET DescricaoProduto = @DescricaoProduto
+                    SET DescricaoProduto = @DescricaoProduto,
+                        GestorCompras = @GestorCompras,
+                        Importado = @Importado
                     WHERE ID = @Id";
 
                 using (var cmdProduto = new SqlCommand(queryProduto, conn))
                 {
                     cmdProduto.Parameters.AddWithValue("@Id", ProdutoDetails.ID);
                     cmdProduto.Parameters.AddWithValue("@DescricaoProduto", ProdutoDetails.DescricaoProduto);
+                    cmdProduto.Parameters.AddWithValue("@GestorCompras", ProdutoDetails.GestorCompras);
+                    cmdProduto.Parameters.AddWithValue("@Importado", ProdutoDetails.Importado);
 
                     await conn.OpenAsync();
                     await cmdProduto.ExecuteNonQueryAsync();
@@ -109,16 +117,19 @@ namespace PortalArcomix.Pages
                 {
                     var queryFornecedor = @"
                         UPDATE Tbl_Fornecedor
-                        SET RazaoSocial = @RazaoSocial
+                        SET RazaoSocial = @RazaoSocial,
+                            TipoFornecedor = @TipoFornecedor
                         WHERE CNPJ = @CNPJ";
 
                     using (var cmdFornecedor = new SqlCommand(queryFornecedor, conn))
                     {
                         cmdFornecedor.Parameters.AddWithValue("@CNPJ", ProdutoDetails.CNPJ);
                         cmdFornecedor.Parameters.AddWithValue("@RazaoSocial", FornecedorDetails.RazaoSocial);
+                        cmdFornecedor.Parameters.AddWithValue("@TipoFornecedor", FornecedorDetails.TipoFornecedor);
 
                         await conn.OpenAsync();
                         await cmdFornecedor.ExecuteNonQueryAsync();
+                        await conn.CloseAsync();
                     }
                 }
             }
@@ -130,15 +141,15 @@ namespace PortalArcomix.Pages
         {
             public int ID { get; set; }
             public string? DescricaoProduto { get; set; }
-            public string? CNPJ { get; set; } // Use CNPJ as the link
+            public string? CNPJ { get; set; }
+            public string? GestorCompras { get; set; } // New property
+            public bool? Importado { get; set; } // New property
         }
 
         public class Fornecedor
         {
             public string? RazaoSocial { get; set; }
+            public string? TipoFornecedor { get; set; } // New property
         }
     }
 }
-
-
-
