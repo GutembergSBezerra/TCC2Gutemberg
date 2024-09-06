c1aae43b4de5f58aeba2a3c6d2e283471e97d6f7
Gutemberg dos Santos Bezerra <papelpoint.contato@hotmail.com>
2024-09-06 19:21:12 -0300

    Atualização para o TCC2

--- Data/OracleDbContext.cs
+++ Data/OracleDbContext.cs
@@ -12,22 +12,7 @@ namespace PortalArcomix.Data
 
         // DbSet for Fornecedor
         public DbSet<Tbl_Usuario> Tbl_Usuario { get; set; }
-        public DbSet<Tbl_Fornecedor> Tbl_Fornecedor { get; set; }
-        public DbSet<Tbl_FornecedorDadosBancarios> Tbl_FornecedorDadosBancarios { get; set; }
-        public DbSet<Tbl_FornecedorContatos> Tbl_FornecedorContatos { get; set; }
-        public DbSet<Tbl_FornecedorNegociacao> Tbl_FornecedorNegociacao { get; set; }
-        public DbSet<Tbl_FornecedorSegurancaAlimentos> Tbl_FornecedorSegurancaAlimentos { get; set; }
-        public DbSet<Tbl_FornecedorDocumentos> Tbl_FornecedorDocumentos { get; set; }
-        public DbSet<Tbl_FornecedorComentarios> Tbl_FornecedorComentarios { get; set; }
-
-
-
-        // DbSet for Produto
-        public DbSet<Tbl_Produto> Tbl_Produto { get; set; }
-        public DbSet<Tbl_ProdutoVendaCompra> Tbl_ProdutoVendaCompra { get; set; }
-        public DbSet<Tbl_ProdutoSubEmbalagem> Tbl_ProdutoSubEmbalagem { get; set; }
-        public DbSet<Tbl_ProdutoComentarios> Tbl_ProdutoComentarios { get; set; }
-
+     
 
 
 
@@ -35,30 +20,7 @@ namespace PortalArcomix.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // Configure the unique constraint on CNPJ for Tbl_FornecedorDadosBancarios
-            modelBuilder.Entity<Tbl_FornecedorDadosBancarios>()
-                .HasIndex(b => b.CNPJ)
-                .IsUnique();
-
-            // Configure the unique constraint on CNPJ for Tbl_FornecedorContatos
-            modelBuilder.Entity<Tbl_FornecedorContatos>()
-                .HasIndex(c => c.CNPJ)
-                .IsUnique();
-
-            // Configure the unique constraint on CNPJ for Tbl_FornecedorNegociacao
-            modelBuilder.Entity<Tbl_FornecedorNegociacao>()
-                .HasIndex(n => n.CNPJ)
-                .IsUnique();
-
-            // Configure the unique constraint on CNPJ for Tbl_FornecedorSegurancaAlimentos
-            modelBuilder.Entity<Tbl_FornecedorSegurancaAlimentos>()
-                .HasIndex(sa => sa.CNPJ)
-                .IsUnique();
-
-            // Map Tbl_FornecedorDocumentos to the database table
-            modelBuilder.Entity<Tbl_FornecedorDocumentos>().ToTable("TBL_FORNECEDORDOCUMENTOS");
-
-
+    
 
         }
     }
